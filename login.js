4cf01abbd0bf13044208f64661519d0924858920
Ranjeet <ranjeet.ggi@gmail.com>
2022-12-23 22:43:51 +0530

    hlo

---
+++ login.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("registerpage");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+    var y = document.getElementById("loginpage");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
